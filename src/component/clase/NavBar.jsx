e0d381aa1b770167d017be796359dd94cb0595cc
dsfyer <Barbatomateo@gmail.com>
2022-05-31 20:14:35 -0400

    algunos cambios a app.css y agregue el componente header

---
+++ src/component/clase/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const NavBar = () =>{
+// PUEDO EJECUTAR JAVASCRIPT ACA
+    return <header>
+    <h1>
+      Jaboon
+    </h1>
+    <nav>
+      <ul className="nav-ul">
+        <li>
+          Jabones
+        </li>
+        <li>
+          Miscelaneos
+        </li>
+        <li>
+          Nosotros
+        </li>
+        <li>
+          Carrito
+        </li>
+      </ul>
+    </nav>
+  </header>
+}
