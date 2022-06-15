ba92937cf626fb9f2ad5748f75c0367154b662cf
dsfyer <Barbatomateo@gmail.com>
2022-06-15 14:51:02 -0400

    nuevo commit

--- src/component/ItemDetail.jsx
+++ src/component/ItemDetail.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import imageurl from '../soapItem.png'
-const ItemDetail = ({name='Cargando',precio='120',description='Cargando',stock='Cargando',initial='Cargando'}) => {
+const ItemDetail = ({item}) => {
                     // 
                     // name,precio,description,stock,initial
-    console.log(name,precio,description,stock,initial)
+    console.log(item)
  
 
     // PONERLE SOMBRAS AL CSS? PUEDE QUEDAR BIEN PARA DESPEGARLO DEL FONDO BLANCO
@@ -14,13 +14,13 @@ const ItemDetail = ({name='Cargando',precio='120',description='Cargando',stock='
                     <img src={imageurl} alt="" />
                 </div>
                 <div className="itemDetail_info">
-                    <h2 className='itemDetail_info_titulo'>{name}</h2>
+                    <h2 className='itemDetail_info_titulo'>{item.name}</h2>
                     <div className="itemDetail_info_description">
-                    <p>{description}</p>
-                    <p>{description}</p>
+                    <p>{item.description}</p>
+                    <p>{item.description}</p>
                     </div>
                     <div className='itemDetail_info_precio'>
-                    <p>{precio}</p>
+                    <p>{item.precio}</p>
                     <button className='button'>Add to cart</button>
                     </div>
                    
