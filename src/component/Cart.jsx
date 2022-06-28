86e109358998a50d413082337c31ee97ad82d5c4
dsfyer <Barbatomateo@gmail.com>
2022-06-28 13:26:06 -0400

    Retoques finales

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -2,26 +2,27 @@ import React from 'react'
 import {CartContext} from './CartContext'
 import {useContext} from 'react'
 import ItemCart from './ItemCart'
+import EmptyCart from './EmptyCart'
 
 const Cart=()=>{
 
     const {clear,itemsCart} = useContext(CartContext)
 
+
     
 return <>
 
+{itemsCart.length===0
+?<EmptyCart/>
+:<div className="cartContainerBackground">
 <article className='cartContainer'>
 {itemsCart.map(item=><ItemCart key={item.id}item={ item}/>)}
-
-
 <section className='buttonClear'>
 <button onClick={clear} className='button'>Borrar Todo</button>
 </section>
 </article>
-
-
+</div>}
 </>
-
 
 
 }
